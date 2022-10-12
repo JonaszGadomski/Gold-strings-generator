ca5072bfd402d98b9d8611da643e49836e999683
Jonasz Gadomski <gadomskijon@gmail.com>
2022-10-12 17:07:10 +0200

    Final version 1.0

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function calculate() {
     numberList.innerHTML = "<span>n<sub>x</sub></span><br>"+"1 => "+firstNumber.value+"<br>2 => "+secondNumber.value+"<br>";
     phiList.innerHTML = "<span>Φ = n<sub>x</sub>&#xf7 n<sub>x-1</sub></span><br>"+"1 => brak"+"<br>";
     goldString.push(firstNumber.value, secondNumber.value);
-    for (i=2; (i-1)<rowsNumber.value; i++) {
+    for (i=3; (i-1)<rowsNumber.value; i++) {  // it generates consequtive number where every next number is result of addition two earlier numbers
         var goldStringLast = goldString.length-1;
         var goldStringPreLast = goldString.length-2;
         var goldStringLastElement = goldString[goldStringLast];
@@ -22,11 +22,14 @@ function calculate() {
         var suma = parseFloat(goldStringLastElement) + parseFloat(goldStringPreLastElement);
         goldString.push(suma);
 
-
         numberList.innerHTML += i + " => "+ suma + "<br>";
-        phiList.innerHTML += i + " => "+(goldString[goldStringLast]/goldString[goldStringPreLast])+ "<br>";
     }
 
+    for (i=2; (i-1)<rowsNumber.value; i++) {   // it generates phiList, dividing numbers from numberList
+        var suma = parseFloat(goldStringLastElement) + parseFloat(goldStringPreLastElement);
+
+        phiList.innerHTML += i + " => "+(goldString[(i-1)]/goldString[(i-2)])+ "<br>";
+    }
 
 }
 
